a3276fbbf6bae2a3aaebe108d34f0cc8f0c141b8
UtilExe <laymasterlolz@gmail.com>
2020-10-28 17:34:29 +0100

    Day2, exercise task 1..

--- day1_ex/src/App.js
+++ day1_ex/src/App.js
@@ -4,6 +4,26 @@ import upper, {text1,text2, text3} from "./file1";
 import person from "./file2";
 import male, {female, allPersons} from "./file2";
 import Welcome, {MultiWelcome} from "./file3";
+import React, { useState, useEffect } from 'react';
+
+function Example() {
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
 
 const {firstName, email} = person;
 console.log(allPersons);
@@ -38,6 +58,7 @@ function App() {
      <p>{email}</p>
      <h2>Ex3</h2>
      <MultiWelcome/>
+     <Example/>
     </div>
   );
 }
